bf025dc8ac583d732814d4fa88166a9e6c3345ca
vanessarocha2-glitch <vanessa.rocha2@estudante.ifms.edu.br>
2026-02-27 11:37:55 -0400

    ProjetoInicial

---
+++ backend/prisma/migrations/20260225163015_init/migration.sql
@@ -0,0 +1,43 @@
+-- CreateEnum
+CREATE TYPE "TodoPriority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "passwod_hash" TEXT NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "todos" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "descreption" TEXT,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "priority" "TodoPriority" NOT NULL DEFAULT 'MEDIUM',
+    "due_at" TIMESTAMP(3),
+    "completed_at" TIMESTAMP(3),
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "todos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "todos_user_id_idx" ON "todos"("user_id");
+
+-- CreateIndex
+CREATE INDEX "todos_completed_idx" ON "todos"("completed");
+
+-- AddForeignKey
+ALTER TABLE "todos" ADD CONSTRAINT "todos_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
