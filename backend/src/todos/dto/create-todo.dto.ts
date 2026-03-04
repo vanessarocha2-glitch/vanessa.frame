7435879da9c2bdd41bec2fc22c06588409274c1a
vanessarocha2-glitch <vanessa.rocha2@estudante.ifms.edu.br>
2026-03-04 12:25:09 -0400

    ATUALIZAÇÃO

---
+++ backend/src/todos/dto/create-todo.dto.ts
@@ -0,0 +1,21 @@
+import{ TodoPriority } from 'generated/prisma/enums';
+
+
+export class CreateTodoDto {
+title: string;
+description: string;
+completed: boolean;
+priority: TodoPriority;
+duetAt: Date;
+completeAt: Date;
+userId: string;
+createdAt: Date;
+updatedAt: Date;
+}
+
+enum TodoPriority {
+    LOW = "LOW",
+    MEDIUM = "MEDIUM",
+    HIGH = "HIGH",
+}
+
