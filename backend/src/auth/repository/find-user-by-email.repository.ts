9b1f2b326a6733f5af1e7ec627fae3fd46b24fed
vanessarocha2-glitch <vanessa.rocha2@estudante.ifms.edu.br>
2026-05-08 12:18:27 -0400

    contato2

---
+++ backend/src/auth/repository/find-user-by-email.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/shared/database/prisma.database";
+
+
+ @Injectable()
+ export class FindUserByEmailRepository {
+ constructor(private readonly prisma: PrismaService) {}
+
+ async findByEmail(email: string) {
+    return await this.prisma.user.findUnique({where: { email }});
+ }
+ }
